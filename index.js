936ee2924dffc8f45c5ee84b45260eb0fe6a575f
leandrochs <leandrochagask@gmail.com>
2022-05-10 17:52:19 -0300

    feat: requisito 05 adiciona variavel UPSIDEDOWN

--- index.js
+++ index.js
@@ -17,7 +17,8 @@ const strangerThingsService = new StrangerThingsService(
 
 app.use(cors());
 
-const hereIsTheUpsideDown = true;
+// const hereIsTheUpsideDown = true;
+const hereIsTheUpsideDown = process.env.UPSIDEDOWN_MODE;
 
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
